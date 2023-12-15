28677299b0114ebcbb85626560a325aca1c57da9
SeongHun <tjdgns5506@gmail.com>
2023-12-15 20:12:30 +0900

    feat: 백엔드 완성

---
+++ src/main/java/com/exam/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.exam.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+}
---
+++ src/main/java/com/exam/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.exam.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
